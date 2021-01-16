736558bf85a86b0ad034f2f39de2c0c5712d0f8c
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-01-15 19:06:05 -0800

    Make the pinout easy to modify by using an enum class

--- i960SxChipset.ino
+++ i960SxChipset.ino
@@ -11,39 +11,50 @@
 // Pin 6  has the LED on Teensy++ 2.0
 // Pin 13 has the LED on Teensy 3.0
 // give it a name:
-constexpr auto led = 0 ; // technically unused
-constexpr auto reset960 = 1; // output
-constexpr auto resetGPIO = 2; // output
-constexpr auto readyPin = 3; // output
-constexpr auto gpioSelect = 4; // data capture spi
-constexpr auto mosi = 5;       // data capture spi
-constexpr auto miso = 6;	   // spi
-constexpr auto sck = 7;		   // spi
-constexpr auto tx0 = 8;  // reserved for serial
-constexpr auto rx0 = 9;  // reserved for serial
-constexpr auto ale = 10; // input
-constexpr auto _as = 11; // input
-constexpr auto _lock = 12; // bidirectional so make it output
-constexpr auto _blast = 13; // input
-constexpr auto dt_r = 14; // input
-constexpr auto _den = 15; // input
-constexpr auto scl = 16; // reserved
-constexpr auto sda = 17; // reserved
-constexpr auto w_r = 18; // input
-constexpr auto hold = 19; // output
-constexpr auto hlda = 20; // input
-constexpr auto _int3 = 21; // output
-constexpr auto int2 = 22; // output
-constexpr auto int1 = 23; // output
-constexpr auto _int0 = 24; // output
-constexpr auto _be0 = 25; // input
-constexpr auto _be1 = 26; // input
-constexpr auto _a1 = 27; // input
-constexpr auto _a2 = 28; // input
-constexpr auto _a3 = 29; // input
-constexpr auto unused0 = 30; // unused
-constexpr auto unused1 = 31; // unused
+enum class i960Pinout : decltype(A0) {
+	Reset960 = 0, // output
+	ResetGPIO,    // output
+	Ready,		  // output
+	Unused0, 	  // unused
+	GPIOSelect,   // output
+	MOSI,		  // reserved
+	MISO,		  // reserved
+	SCK, 		  // reserved
+	TX0, 		  // reserved
+	RX0, 		  // reserved
+	ALE,		  // input
+	AS_, 		  // input
+	Lock_,		  // bidirectional but default to output
+	Blast_,		  // input
+	DT_R, 		  // input
+	DEN_, 		  // input
+	SCL,		  // reserved
+	SDA, 		  // reserved
+	W_R, 		  // input
+	Hold,		  // output
+	HLDA,         // input
+	Int3_,        // output
+	Int2, 		  // output
+	Int1,		  // output
+	Int0_,		  // output
+	ByteEnable0_, // input
+	ByteEnable1_, // input
+	BurstAddress1, // input
+	BurstAddress2, // input
+	BurstAddress3, // input
+	Unused1, 	   // unused
+	Unused2, 	   // unused
+	Count,		   // special
+	Led = 0,
+};
+static_assert(static_cast<decltype(HIGH)>(i960Pinout::Count) == 32);
 
+inline void digitalWrite(i960Pinout ip, decltype(HIGH) value) {
+	digitalWrite(static_cast<int>(ip), value);
+}
+inline void pinMode(i960Pinout ip, decltype(INPUT) value) {
+	pinMode(static_cast<int>(ip), value);
+}
 template<typename ... Pins>
 void setupPins(decltype(OUTPUT) direction, Pins ... pins) {
 	(pinMode(pins, direction), ...);
@@ -54,34 +65,64 @@ void digitalWriteBlock(decltype(HIGH) value, Pins ... pins) {
 	(digitalWrite(pins, value), ...);
 }
 
-template<int pinId, decltype(HIGH) onConstruction, decltype(LOW) onDestruction>
+template<i960Pinout pinId, decltype(HIGH) onConstruction, decltype(LOW) onDestruction>
 class PinToggler {
 	public:
 		PinToggler() { digitalWrite(pinId, onConstruction); }
 		~PinToggler() { digitalWrite(pinId, onDestruction); }
 };
 
-template<int pinId>
+
+template<i960Pinout pinId>
 using HoldPinLow = PinToggler<pinId, LOW, HIGH>;
+
+template<i960Pinout pinId>
+using HoldPinHigh = PinToggler<pinId, HIGH, LOW>;
 
 // the setup routine runs once when you press reset:
 void setup() {
 	Serial.begin(9600);
-	setupPins(OUTPUT, resetGPIO, reset960);
-	HoldPinLow<reset960> holdi960InReset;
-	HoldPinLow<resetGPIO> gpioReset;
-	setupPins(OUTPUT, led, hold);
-	digitalWriteBlock(LOW, led, hold);
-	setupPins(OUTPUT, readyPin, gpioSelect, _lock, _int3, _int0, led, hold, int2, int1);
-	digitalWriteBlock(HIGH, readyPin, gpioSelect, _lock, _int3, _int0);
-	setupPins(INPUT, ale, _as, _blast, dt_r, _den, w_r, hlda, _be0, _be1, _a1, _a2, _a3);
+	setupPins(OUTPUT, 
+			i960Pinout::ResetGPIO, 
+			i960Pinout::Reset960);
+	HoldPinLow<i960Pinout::Reset960> holdi960InReset;
+	HoldPinLow<i960Pinout::ResetGPIO> gpioReset;
+	setupPins(OUTPUT,
+			i960Pinout::Ready,
+			i960Pinout::GPIOSelect,
+			i960Pinout::Lock_,
+			i960Pinout::Int3_,
+			i960Pinout::Int0_,
+			i960Pinout::Hold,
+			i960Pinout::Int2,
+			i960Pinout::Int1);
+	digitalWriteBlock(HIGH,
+			i960Pinout::Ready,
+			i960Pinout::GPIOSelect,
+			i960Pinout::Lock_,
+			i960Pinout::Int3_,
+			i960Pinout::Int0_);
+	digitalWrite(i960Pinout::Hold, LOW);
+	setupPins(INPUT,
+			i960Pinout::ALE,
+			i960Pinout::AS_,
+			i960Pinout::Blast_,
+			i960Pinout::DT_R,
+			i960Pinout::DEN_,
+			i960Pinout::W_R,
+			i960Pinout::HLDA,
+			i960Pinout::ByteEnable0_,
+			i960Pinout::ByteEnable1_,
+			i960Pinout::BurstAddress1,
+			i960Pinout::BurstAddress2,
+			i960Pinout::BurstAddress3);
 	SPI.begin();
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-	digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+	digitalWrite(i960Pinout::Led, HIGH);   // turn the LED on (HIGH is the voltage level)
 	delay(1000);               // wait for a second
-	digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+	digitalWrite(i960Pinout::Led, LOW);    // turn the LED off by making the voltage LOW
 	delay(1000);               // wait for a second
 }
