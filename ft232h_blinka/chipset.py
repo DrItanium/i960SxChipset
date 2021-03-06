591937b623d74dd4050a505c9873ac1a98d09bde
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-03-06 14:23:28 -0800

    Hold microcontroller in reset until the ft232h is ready

--- ft232h_blinka/chipset.py
+++ ft232h_blinka/chipset.py
@@ -6,16 +6,20 @@ import digitalio
 from adafruit_bus_device.spi_device import SPIDevice
 
 # make a storage block of 1 gigabyte
-i960Memory = bytearray(1024 * 1024 * 1024)
 # zero out memory to make sure it is allocated
+mcuReset = digitalio.DigitalInOut(board.D7)
+mcuReset.direction = digitalio.Direction.OUTPUT
+mcuReset.value = False
+i960Memory = bytearray(1024 * 1024 * 1024)
 with busio.SPI(board.SCK, board.MOSI, board.MISO) as spi:
     cs = digitalio.DigitalInOut(board.D4)
+    ack = digitalio.DigitalInOut(board.D6)
+    ack.direction = digitalio.Direction.OUTPUT
     atmega1284pSync = digitalio.DigitalInOut(board.D5)
     cs.direction = digitalio.Direction.OUTPUT
     cs.value = True
     atmega1284pSync.direction = digitalio.Direction.INPUT
     device = SPIDevice(spi, cs, baudrate=5000000)
-    print("setup done!")
     previousValue = True
     readCommand = bytearray(4)
     readCommand[0] = 0x3
@@ -24,6 +28,7 @@ with busio.SPI(board.SCK, board.MOSI, board.MISO) as spi:
     readCommand[3] = 0x00
     inputBuffer = bytearray(8)
     outputBuffer = bytearray(2)
+    mcuReset.value = True
     while True:
         # keep waiting until sync pin goes low
         if (atmega1284pSync.value != previousValue):
