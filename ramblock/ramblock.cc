003225a01977d54955aa4e67368e2ebcc703d422
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-03-10 17:23:40 -0800

    try catch

--- ramblock/ramblock.cc
+++ ramblock/ramblock.cc
@@ -4,9 +4,16 @@
 
 #include <iostream>
 #include <boost/asio.hpp>
+#include <boost/program_options.hpp>
 
 int main(int argc, char** argv) {
-    boost::asio::io_service service;
-    boost::asio::serial_port sp(service, "/dev/ttyUSB0");
-    return 0;
+    try {
+        boost::asio::io_service service;
+        boost::asio::serial_port sp(service, "/dev/ttyUSB0");
+        return 0;
+    } catch (boost::system::system_error& error) {
+       std::cerr << "ERROR: " << error.what() << std::endl;
+       std::cerr << "Terminating execution!" << std::endl;
+       return 1;
+    }
 }
