59d5f6729386b8672b8a27db563d5b1b6957834c
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-03-10 17:18:57 -0800

    define the ramblock program
    It will run on the host and communicate with the atmega1284p over it's serial connection. All memory requests will go through the Serial object on the arduino. Very simple design, not fast but easy to diagnose and implement. Will use boost::asio libraries on host

--- ramblock/ramblock.cc
+++ ramblock/ramblock.cc
@@ -2,3 +2,10 @@
 // Created by jwscoggins on 3/10/21.
 //
 
+#include <iostream>
+#include <boost/asio/serial_port.hpp>
+
+int main(int argc, char** argv) {
+    boost::asio::serial_port sp("foo");
+    return 0;
+}
