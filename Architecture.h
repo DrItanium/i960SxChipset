e39c28d976f276aed0c0650ecbc1cf4346ad3724
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-03-19 15:39:35 -0700

    Implement basic support

--- Architecture.h
+++ Architecture.h
@@ -4,5 +4,34 @@
 
 #ifndef ARCHITECTURE_H
 #define ARCHITECTURE_H
+enum class CPUArchitecture {
+    Unknown = 0,
+    ARM,
+    AVR,
+};
 
+constexpr auto architectureIsArm() noexcept {
+#if defined(__arm__) || defined(__thumb__)
+    return true;
+#else
+    return false;
+#endif
+}
+constexpr auto architectureIsAVR() noexcept {
+#if defined(__AVR) || defined(AVR)
+    return true;
+#else
+    return false;
+#endif
+}
+
+constexpr auto getArchitecture() noexcept {
+    if constexpr (architectureIsAVR()) {
+        return CPUArchitecture::AVR;
+    } else if constexpr (architectureIsArm()) {
+        return CPUArchitecture::ARM;
+    } else {
+        return CPUArchitecture::Unknown;
+    }
+}
 #endif //ARCHITECTURE_H
