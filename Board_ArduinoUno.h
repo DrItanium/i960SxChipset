5e1fda9c0ca9ba1c5e7b694296fea31fa4de854c
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-03-19 15:09:21 -0700

    Cleanup pinout

--- Board_ArduinoUno.h
+++ Board_ArduinoUno.h
@@ -7,28 +7,32 @@
 #include <Arduino.h>
 #include "BoardSpecific.h"
 enum class i960Pinout : decltype(A0) {
-    Led = LED_BUILTIN,
-    SCL = A5,
-    SDA = A4,
-    AS_ = 2,
-    DEN_ = 3,
-    Ready = 4,
-    Int0_ = 5,
-    W_R_ = 6,
-    Reset960 = 7,
-    BLAST_ = 8,
-    FAIL = 9,
-    GPIOSelect = 10,
-    MOSI = 11,
-    MISO = 12,
-    SCK = 13,
+    RX0 = 0,
+    TX0,
+    AS_,
+    DEN_,
+    Ready,
+    Int0_,
+    W_R_,
+    Reset960,
+    BLAST_,
+    FAIL,
+    GPIOSelect,
+    MOSI,
+    MISO,
+    SCK,
+    Count,
     SPI_BUS_EN = A0,
     Analog1 = A1,
     Analog2 = A2,
     Analog3 = A3,
     Analog4 = A4,
     Analog5 = A5,
+    SCL = A5,
+    SDA = A4,
+    Led = LED_BUILTIN,
 };
+static_assert(static_cast<i960Pinout::Count> == 14);
 #define AS_ISR INT0_vect
 #define DEN_ISR INT1_vect
 #endif //ARDUINO_BOARD_ARDUINOUNO_H
