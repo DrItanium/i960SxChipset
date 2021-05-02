52d55b84b5a6ad87b0c1fe741c86323b8fa44fd2
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-05-02 14:57:06 -0700

    Start a separate folder without nindy to make a shim boot process

---
+++ i960/hitagimon/sx/f_handle.c
@@ -0,0 +1,77 @@
+/*
+i960SxChipset
+Copyright (c) 2020-2021, Joshua Scoggins
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/*
+ * Fault handler routines
+ */
+
+extern void user_reserved(void);
+extern void user_trace(void);
+extern void user_operation(void);
+extern void user_arithmetic(void);
+extern void user_real_arithmetic(void);
+extern void user_constraint(void);
+extern void user_protection(void);
+extern void user_machine(void);
+extern void user_type(void);
+
+void
+user_reserved(void) {
+
+}
+
+void
+user_trace(void) {
+
+}
+
+void
+user_operation(void) {
+
+}
+void
+user_arithmetic(void) {
+
+}
+void
+user_real_arithmetic(void) {
+
+}
+void
+user_constraint(void) {
+
+}
+void
+user_protection(void) {
+
+}
+void
+user_machine(void) {
+
+}
+void
+user_type(void) {
+
+}
