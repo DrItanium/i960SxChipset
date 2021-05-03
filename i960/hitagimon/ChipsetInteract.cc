4723a964a44653b768e5c18461c60c8fe2ee5ba5
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-05-02 18:33:45 -0700

    Define IO Base addresses

--- i960/hitagimon/ChipsetInteract.cc
+++ i960/hitagimon/ChipsetInteract.cc
@@ -5,3 +5,4 @@
 #include "ChipsetInteract.h"
 #include "IORoutines.h"
 
+
--- i960/hitagimon/ChipsetInteract.h
+++ i960/hitagimon/ChipsetInteract.h
@@ -4,6 +4,8 @@
 
 #ifndef I960SXCHIPSET_PERIPHERALS_H
 #define I960SXCHIPSET_PERIPHERALS_H
-#include <cstdint.h>
-byte* getLEDAddress();
+#include <stdint.h>
+inline uint32_t getIOBase0Address() { return 0xFE000000; }
+inline uint32_t getCPUInternalMemoryBaseAddress() { return 0xFF000000; }
+
 #endif //I960SXCHIPSET_PERIPHERALS_H
