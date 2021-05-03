05c8c3cc999d1949ce5900cbc77925f928835cbf
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-05-03 11:19:49 -0700

    Pull in string and iostream for testing purposes
    Turns out that libnosys is basically a stub library which just sets appropriate errno flags. Removing no sys from the list of libraries causes the following undefined functions to popup:
    
     - fstat
     - open
     - read
     - lseek
     - write
     - close
     - isatty
    
     I will need to implement these functions myself so that they link to the chipset where appropriate.

--- i960/hitagimon/brk.cc
+++ i960/hitagimon/brk.cc
@@ -6,5 +6,5 @@
 
 extern "C"
 int brk(void* addr) {
-    return ENOMEM;
+    return -1;
 }
--- i960/hitagimon/main.cc
+++ i960/hitagimon/main.cc
@@ -1,9 +1,12 @@
 //
 // Created by jwscoggins on 5/2/21.
 //
-
+#include <string>
+#include <iostream>
 
 int main() {
+    std::string donuts("donuts");
+    std::cout << donuts << std::endl;
     return 0;
 }
 
