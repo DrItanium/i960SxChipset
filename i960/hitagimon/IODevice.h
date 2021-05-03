835938b9e59803632a25b0e2f40519f5c324c16f
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-05-03 09:44:19 -0700

    Drop IODevice for now

--- i960/hitagimon/IODevice.h
+++ i960/hitagimon/IODevice.h
@@ -6,13 +6,5 @@
 #define I960SXCHIPSET_IODEVICE_H
 #include <stdint.h>
 
-class IODevice {
-public:
-    IODevice(uint8_t* baseAddress);
-    IODevice(uint32_t byteOffset);
-    virtual ~IODevice();
-protected:
-    uint8_t* baseAddress_;
-};
 
 #endif //I960SXCHIPSET_IODEVICE_H
