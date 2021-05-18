ec401c5b3ec1cbabb93f0c87a2533a8c519a30b2
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-05-18 15:42:35 -0700

    Make it possible to differentiate code

---
+++ include/MCUPlatform.h
@@ -0,0 +1,41 @@
+// contains routines for handling microcontroller platform detection
+// Created by jwscoggins on 5/18/21.
+//
+
+#ifndef I960SXCHIPSET_MCUPLATFORM_H
+#define I960SXCHIPSET_MCUPLATFORM_H
+#include <cstdint>
+enum class TargetMCU : uint8_t {
+    ATmega1284p,
+    GrandCentralM4,
+    NRF52830,
+    Unknown,
+};
+[[nodiscard]] constexpr auto getMCUTarget() noexcept {
+#ifdef ARDUINO_AVR_ATmega1284
+    return TargetMCU::ATmega1284p;
+#elif defined(ARDUINO_GRAND_CENTRAL_M4)
+    return TargetMCU::GrandCentralM4;
+#elif defined(NRF52832_XXAA)
+    return TargetMCU::NRF52830;
+    // @todo add more targets here
+#else
+    return TargetMCU::Unknown;
+#endif
+}
+[[nodiscard]] constexpr auto onAtmega1284p() noexcept {
+    return getMCUTarget() == TargetMCU::ATmega1284p;
+}
+[[nodiscard]] constexpr auto onGrandCentralM4() noexcept {
+    return getMCUTarget() == TargetMCU::GrandCentralM4;
+}
+
+[[nodiscard]] constexpr auto onNRF52830() noexcept {
+    return getMCUTarget() == TargetMCU::NRF52830;
+}
+
+[[nodiscard]] constexpr auto onUnknownTarget() noexcept {
+    return getMCUTarget() == TargetMCU::Unknown;
+}
+
+#endif //I960SXCHIPSET_MCUPLATFORM_H
