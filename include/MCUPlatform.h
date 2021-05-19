18dd26a281434608c7c67d20d661860f0ba86dd3
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-05-19 14:30:57 -0700

    feather m0 targets are linked up

--- include/MCUPlatform.h
+++ include/MCUPlatform.h
@@ -4,10 +4,19 @@
 
 #ifndef I960SXCHIPSET_MCUPLATFORM_H
 #define I960SXCHIPSET_MCUPLATFORM_H
+#ifdef ARDUINO_SAMD_FEATHER_M0
+#ifdef HAS_BUILTIN_SDCARD
+#define ADAFRUIT_FEATHER_M0_ADALOGGER
+#else /* !defined(HAS_BUILTIN_SDCARD) */
+#define ADAFRUIT_FEATHER_M0_BASIC
+#endif
+#endif
 enum class TargetMCU {
     ATmega1284p,
     GrandCentralM4,
-    NRF52830,
+    FeatherNRF52830,
+    FeatherM0Basic,
+    FeatherM0Adalogger,
     Unknown,
 };
 [[nodiscard]] constexpr auto getMCUTarget() noexcept {
@@ -16,8 +25,12 @@ enum class TargetMCU {
 #elif defined(ARDUINO_GRAND_CENTRAL_M4)
     return TargetMCU::GrandCentralM4;
 #elif defined(ARDUINO_NRF52_ADAFRUIT)
-    return TargetMCU::NRF52830;
+    return TargetMCU::FeatherNRF52830;
     // @todo add more targets here
+#elif defined(ADAFRUIT_FEATHER_M0_ADALOGGER)
+    return TargetMCU::FeatherM0Adalogger;
+#elif defined(ADAFRUIT_FEATHER_M0_BASIC)
+    return TargetMCU::FeatherM0Basic;
 #else
     return TargetMCU::Unknown;
 #endif
@@ -29,12 +42,25 @@ enum class TargetMCU {
     return getMCUTarget() == TargetMCU::GrandCentralM4;
 }
 
-[[nodiscard]] constexpr auto onNRF52830() noexcept {
-    return getMCUTarget() == TargetMCU::NRF52830;
+[[nodiscard]] constexpr auto onFeatherNRF52830() noexcept {
+    return getMCUTarget() == TargetMCU::FeatherNRF52830;
 }
 
 [[nodiscard]] constexpr auto onUnknownTarget() noexcept {
     return getMCUTarget() == TargetMCU::Unknown;
+}
+
+[[nodiscard]] constexpr auto onFeatherM0Basic() noexcept {
+    return getMCUTarget() == TargetMCU::FeatherM0Basic;
+}
+
+[[nodiscard]] constexpr auto onFeatherM0Adalogger() noexcept {
+    return getMCUTarget() == TargetMCU::FeatherM0Adalogger;
+}
+
+[[nodiscard]] constexpr auto onFeatherM0() noexcept {
+    return onFeatherM0Basic() ||
+           onFeatherM0Adalogger();
 }
 
 #endif //I960SXCHIPSET_MCUPLATFORM_H
