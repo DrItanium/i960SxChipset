3293169ace6088d7a11c02876274bca207d384bc
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-05-22 11:20:34 -0700

    Expose the led to the i960

--- i960/hitagimon/main.cc
+++ i960/hitagimon/main.cc
@@ -3,10 +3,24 @@
 //
 #include <string>
 #include <iostream>
-
+#include "IODevice.h"
+BuiltinLED theLed(0);
+int wait(int count) {
+    int result = 0;
+    for (int i = 0; i < count; ++i) {
+        result += i;
+    }
+    return result;
+}
 int main() {
     std::string donuts("donuts");
     std::cout << donuts << std::endl;
+    theLed.setValue(false);
+    wait(1000);
+    while(true) {
+        theLed.toggle();
+        wait(1000000);
+    }
     return 0;
 }
 
