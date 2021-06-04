3f7afe9ef52663a01909038023b77541ededd78c
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-06-04 12:04:05 -0700

    Start defining memory thing objects
    Assists us in adding and maintaining io devices and other components attached to the memory bus

--- src/i960SxChipset.cpp
+++ src/i960SxChipset.cpp
@@ -126,7 +126,6 @@ uint32_t buttonsCache = 0;
 // A four address is used to act as a door bell!
 // storage area for the display + a doorbell address as well
 // the command is setup to send off to the display
-
 template<typename DisplayType>
 class DisplayCommand {
 public:
@@ -153,9 +152,6 @@ public:
         GetRotation,
         GetCursorX,
         GetCursorY,
-        WritePixel,
-        WriteColor,
-        WriteFillRect,
         DrawPixel,
         Color565,
         DrawRoundRect,
@@ -244,7 +240,7 @@ public:
     }
 public:
     /// @todo extend BusDevice and take in a base address
-    DisplayCommand(DisplayType& display) : display_(display) { }
+    explicit DisplayCommand(DisplayType& display) : display_(display) { }
     [[nodiscard]] constexpr auto getCommand() const noexcept { return command_; }
     [[nodiscard]] constexpr auto getX() const noexcept { return x_; }
     [[nodiscard]] constexpr auto getY() const noexcept { return y_; }
@@ -441,6 +437,89 @@ constexpr MemoryMapEntry getMemoryMapEntry(MemoryMapAddresses address) noexcept 
 constexpr Address memoryMapAddress(MemoryMapAddresses address) noexcept {
     return getMemoryMapEntry(address).value_;
 }
+/**
+ * @brief Describes the interface between a component and a memory request, it introduces some latency with the trade off being easier maintenance
+ */
+class MemoryThing {
+public:
+    virtual ~MemoryThing() = default;
+    [[nodiscard]] virtual uint8_t read8(Address address) noexcept = 0;
+    [[nodiscard]] virtual uint16_t read16(Address address) noexcept = 0;
+    [[nodiscard]] virtual bool respondsTo(Address address) const noexcept = 0;
+    virtual void write8(Address address, uint8_t value) noexcept = 0;
+    virtual void write16(Address address, uint16_t value) noexcept = 0;
+};
+/**
+ * @brief Describes a thing connected to the memory bus which responds to a range of addresses
+ */
+class MemoryRangeThing : public MemoryThing {
+public:
+    MemoryRangeThing(Address baseAddress, Address endAddress) : base_(baseAddress), end_(endAddress), length_(endAddress - baseAddress) { }
+    ~MemoryRangeThing() override = default;
+    [[nodiscard]] bool respondsTo(Address address) const noexcept override {
+        return base_ <= address && address < end_;
+    }
+    [[nodiscard]] constexpr Address getBase() const noexcept { return base_; }
+    [[nodiscard]] constexpr Address getEnd() const noexcept { return end_; }
+    [[nodiscard]] constexpr Address getLength() const noexcept { return length_; }
+private:
+    Address base_;
+    Address end_;
+    Address length_;
+};
+
+/**
+ * @brief Describes a thing which responds to a single address
+ */
+class MemoryRegisterThing : public MemoryThing {
+public:
+    explicit MemoryRegisterThing(Address address) noexcept : theAddress_(address) { }
+    ~MemoryRegisterThing() override = default;
+    [[nodiscard]] bool respondsTo(Address value) const noexcept override { return value == theAddress_; }
+    [[nodiscard]] constexpr auto getAddress() const noexcept { return theAddress_; }
+private:
+    Address theAddress_;
+};
+
+class LambdaMemoryRegisterThing : public MemoryRegisterThing {
+public:
+    using Read8Operation = uint8_t(*)(Address);
+    using Read16Operation = uint16_t(*)(Address);
+    using Write8Operation = void(*)(Address, uint8_t);
+    using Write16Operation = void(*)(Address, uint16_t);
+public:
+    LambdaMemoryRegisterThing(Address base, Read8Operation r8, Write8Operation w8, Read16Operation r16, Write16Operation w16) noexcept : MemoryRegisterThing(base), read8_(r8), read16_(r16), write8_(w8), write16_(w16) { }
+    LambdaMemoryRegisterThing(Address base, Read8Operation r8, Write8Operation w8) noexcept : LambdaMemoryRegisterThing(base, r8, w8, nullptr, nullptr) { }
+    LambdaMemoryRegisterThing(Address base, Read16Operation r16, Write16Operation w16) noexcept : LambdaMemoryRegisterThing(base, nullptr, nullptr, r16, w16) { }
+    ~LambdaMemoryRegisterThing() override = default;
+    uint8_t read8(Address address) noexcept override {
+        if (read8_) {
+            return read8_(address);
+        }
+        return 0;
+    }
+    uint16_t read16(Address address) noexcept override {
+        if (read16_) {
+            return read16_(address);
+        }
+        return 0;
+    }
+    void write8(Address address, uint8_t value) noexcept override {
+        if (write8_) {
+            write8_(address, value);
+        }
+    }
+    void write16(Address address, uint16_t value) noexcept override {
+        if (write16_) {
+            write16_(address, value);
+        }
+    }
+private:
+    Read8Operation read8_;
+    Read16Operation read16_;
+    Write8Operation write8_;
+    Write16Operation write16_;
+};
 DisplayCommand<decltype(tft)> displayCommandSet(tft);
 
 // for the feather m0 only
