d8aff363e923f88cf460c34a7015a4bcdc449781
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-06-16 21:16:52 -0700

    More includes

--- include/OPL2Thing.h
+++ include/OPL2Thing.h
@@ -4,12 +4,16 @@
 
 #ifndef I960SXCHIPSET_OPL2THING_H
 #define I960SXCHIPSET_OPL2THING_H
+#include <Arduino.h>
 #include "MemoryThing.h"
 #include <OPL2.h>
 class OPL2Thing : public IOSpaceThing {
 public:
     explicit OPL2Thing(Address base) : IOSpaceThing(base, base + 0x100) { }
+    ~OPL2Thing() override = default;
+    void begin() noexcept override {
 
+    }
 private:
     OPL2 theOPL2_;
 };
