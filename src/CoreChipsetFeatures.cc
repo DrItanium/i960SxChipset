e883096969a4a3ce86f4d822355a0e26261a8936
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-06-23 13:54:32 -0700

    Turn off debugging in the copy engine

--- src/CoreChipsetFeatures.cc
+++ src/CoreChipsetFeatures.cc
@@ -213,7 +213,7 @@ CoreChipsetFeatures::invokeCopyEngine() noexcept {
             Serial.println(F("]"));
         }
         for (uint32_t i = 0; i < fullCopies; ++i, srcAddrPtr += CopyEngineCacheSize, destAddrPtr += CopyEngineCacheSize) {
-            if constexpr (true) {
+            if constexpr (false) {
                 Serial.print(F("COPYING FROM 0x"));
                 Serial.print(srcAddrPtr, HEX);
                 Serial.print(F(" TO 0x"));
