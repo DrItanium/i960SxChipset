8f013416b793042a5246964cbfff9d5e403cb3f3
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-06-30 10:23:27 -0700

    De loopify the code

--- src/i960SxChipset.cpp
+++ src/i960SxChipset.cpp
@@ -625,26 +625,24 @@ void waitTillNexti960SxCycle() noexcept {
 #endif
 }
 void processDataRequest() noexcept {
-    for (int i = 0; i < 16; ++i) {
-        processorInterface.updateDataCycle();
-        // do not allow writes or reads into processor internal memory
-        Address burstAddress = processorInterface.getAddress();
-        LoadStoreStyle style = processorInterface.getStyle();
-        if (processorInterface.isReadOperation()) {
-            processorInterface.setDataBits(theThing->read(burstAddress, style));
-        } else {
-            theThing->write(burstAddress, processorInterface.getDataBits(), style);
-        }
-        // setup the proper address and emit this over serial
-        processorInterface.signalReady();
-        if (processorInterface.blastTriggered()) {
-            // we not in burst mode
-            fsm.trigger(ReadyAndNoBurst);
-            return;
-        } else {
-            if constexpr (!TargetBoard::onAtmega1284p()) {
-                waitTillNexti960SxCycle();
-            }
+    processorInterface.updateDataCycle();
+    // do not allow writes or reads into processor internal memory
+    Address burstAddress = processorInterface.getAddress();
+    LoadStoreStyle style = processorInterface.getStyle();
+    if (processorInterface.isReadOperation()) {
+        processorInterface.setDataBits(theThing->read(burstAddress, style));
+    } else {
+        theThing->write(burstAddress, processorInterface.getDataBits(), style);
+    }
+    // setup the proper address and emit this over serial
+    processorInterface.signalReady();
+    if (processorInterface.blastTriggered()) {
+        // we not in burst mode
+        fsm.trigger(ReadyAndNoBurst);
+        return;
+    } else {
+        if constexpr (!TargetBoard::onAtmega1284p()) {
+            waitTillNexti960SxCycle();
         }
     }
 }
