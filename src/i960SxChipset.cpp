626fef44754d68d5e824b586beb06f82235b1088
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-06-30 21:47:50 -0700

    Check isReadOperation once outside the loop

--- src/i960SxChipset.cpp
+++ src/i960SxChipset.cpp
@@ -607,12 +607,13 @@ void loop() {
         Serial.println(processorInterface.getAddress(), HEX);
         signalHaltState(F("UNMAPPED MEMORY REQUEST!"));
     }
+    auto isReadOperation = DigitalPin<i960Pinout::W_R_>::isAsserted();
     do {
         processorInterface.updateDataCycle();
         auto blastAsserted = DigitalPin<i960Pinout::BLAST_>::isAsserted();
         Address burstAddress = processorInterface.getAddress();
         LoadStoreStyle style = processorInterface.getStyle();
-        if (DigitalPin<i960Pinout::W_R_>::isAsserted()) {
+        if (isReadOperation) {
             processorInterface.setDataBits(theThing->read(burstAddress, style));
         } else {
             theThing->write(burstAddress, processorInterface.getDataBits(), style);
