7b16401e74eef5a25746f2535a0ad528d55cdbda
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-07-27 21:40:25 -0700

    Introduce the clock setup code

--- src/i960SxChipset.cpp
+++ src/i960SxChipset.cpp
@@ -1056,7 +1056,23 @@ void purgeSRAMCache() noexcept {
 #endif
 // the setup routine runs once when you press reset:
 void setup() {
+    constexpr auto ClockDivider_10MHZ = 6;
+    constexpr auto ClockDivider_12MHZ = 5;
+    constexpr auto ClockDivider_15MHZ = 4;
+    constexpr auto ClockDivider_20MHZ = 3;
+    //constexpr auto ClockDivider_40MHZ = 2; // THIS IS ALSO DAMN DANGEROUS
+    //constexpr auto ClockDivider_80MHZ = 1; // DEAR GOD DO NOT USE THIS!!!!
+    GCLK->GENCTRL[3].reg = GCLK_GENCTRL_DIV(ClockDivider_10MHZ) |
+                           GCLK_GENCTRL_IDC |
+                           GCLK_GENCTRL_GENEN |
+                           GCLK_GENCTRL_OE |
+                           GCLK_GENCTRL_SRC_DPLL0;
+    while (GCLK->SYNCBUSY.bit.GENCTRL3);
 
+    constexpr auto pinIndex0 = static_cast<int>(i960Pinout::CLOCK_OUT);
+    PORT->Group[g_APinDescription[pinIndex0].ulPort].PINCFG[g_APinDescription[pinIndex0].ulPin].bit.PMUXEN = 1;
+    // enable on pin 36 or PA17
+    PORT->Group[g_APinDescription[pinIndex0].ulPort].PMUX[g_APinDescription[pinIndex0].ulPin >> 1].reg |= PORT_PMUX_PMUXO(MUX_PA17M_GCLK_IO3);
     Serial.begin(115200);
     while(!Serial) {
         delay(10);
