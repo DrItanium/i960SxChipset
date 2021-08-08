5887bdaaa940e7f14bb7f3ba37c424654869abd8
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-08-08 13:30:48 -0700

    Define the PSRAM Block

--- include/PSRAMChip.h
+++ include/PSRAMChip.h
@@ -209,6 +209,175 @@ private:
 private:
     bool available_ = true;
 };
+/**
+ * @brief Wraps eight different SPI sram chips into a single object
+ * @tparam enablePin The pin that is used to signal chip usage
+ * @tparam sel0 The lowest pin used to select the target device
+ * @tparam sel1 The middle pin used to select the target device
+ * @tparam sel2 The upper pin used to select the target device
+ */
+template<i960Pinout enablePin,
+        i960Pinout sel0 = i960Pinout::SPI_OFFSET0,
+        i960Pinout sel1 = i960Pinout::SPI_OFFSET1,
+        i960Pinout sel2 = i960Pinout::SPI_OFFSET2>
+class PSRAMBlock8 : public MemoryThing {
+public:
+    static constexpr auto EnablePin = enablePin;
+    static constexpr auto Select0 = sel0;
+    static constexpr auto Select1 = sel1;
+    static constexpr auto Select2 = sel2;
+    using SingleChip = PSRAMChip<EnablePin>;
+    static constexpr auto NumChips = 8;
+    static constexpr auto Size = NumChips * SingleChip::Size;
+    static constexpr auto Mask = Size - 1;
+    static constexpr auto SingleChipSize = SingleChip::Size;
+    static_assert ((EnablePin != Select0) && (EnablePin != Select1) && (EnablePin != Select2), "The enable pin must be different from all select pins");
+    static_assert ((Select0 != Select1) && (Select0 != Select2) && (Select1 != Select2), "All three select pins must point to a different physical pin");
+    static_assert(Size == 64_MB, "PSRAMBlock8 needs to be 1 megabyte in size");
+    static_assert(Mask == 0x0FFF'FFFF, "PSRAMBlock8 mask is wrong!");
+public:
+    explicit PSRAMBlock8(Address base) : MemoryThing(base, base + Size),
+    backingChips{
+                SingleChip (base + (0 * SingleChipSize)),
+                SingleChip (base + (1 * SingleChipSize)),
+                SingleChip (base + (2 * SingleChipSize)),
+                SingleChip (base + (3 * SingleChipSize)),
+                SingleChip (base + (4 * SingleChipSize)),
+                SingleChip (base + (5 * SingleChipSize)),
+                SingleChip (base + (6 * SingleChipSize)),
+                SingleChip (base + (7 * SingleChipSize)),
+    } {
+
+    }
+    ~PSRAMBlock8() override = default;
+    union Address26 {
+        constexpr explicit Address26(Address value = 0) : base(value) { }
+        constexpr auto getAddress() const noexcept { return base; }
+        constexpr auto getOffset() const noexcept { return offset; }
+        constexpr auto getIndex() const noexcept { return index; }
+        Address base : 26; // 1 megabyte always
+        struct {
+            Address offset : 23;
+            Address index : 3;
+        };
+    };
+
+    size_t blockWrite(Address address, uint8_t *buf, size_t capacity) noexcept override {
+        // okay figure out which device to start the write into, since size_t is 16 bits on AVR we can exploit this for our purposes.
+        // There is no way to go beyond a 64k buffer (which to be honest would be crazy anyway since this chip doesn't have that much sram)
+        // because of that we can just do a write to the underlying device and find out how much is left over to continue from that point
+        Address26 curr(address);
+        Address26 end(address + capacity);
+        size_t bytesRemaining = capacity;
+        size_t bytesWritten = 0;
+        auto currentAddress = address;
+        auto* theBuf = buf;
+        // just keep spanning the whole area as we go along
+        for (byte i = curr.getIndex(); i <= end.getIndex(); ++i) {
+            setChipId(i);
+            auto numWritten = backingChips[i].write(currentAddress, theBuf, bytesRemaining);
+            bytesWritten += numWritten;
+            bytesRemaining -= numWritten;
+            theBuf += numWritten;
+            currentAddress += numWritten;
+        }
+        return bytesWritten;
+    }
+    size_t blockRead(Address address, uint8_t *buf, size_t capacity) noexcept override {
+        // just like blockWrite, we can span multiple devices and thus we just need to keep populating the buffer as we go along
+        Address26 curr(address);
+        Address26 end(address + capacity);
+        size_t bytesRemaining = capacity;
+        size_t bytesRead = 0;
+        auto* theBuf = buf;
+        auto currentAddress = address;
+        // just keep spanning the whole area as we go along
+        for (byte i = curr.getIndex(); i <= end.getIndex(); ++i) {
+            setChipId(i);
+            auto numRead = backingChips[i].read(currentAddress, theBuf, bytesRemaining);
+            bytesRead += numRead;
+            bytesRemaining -= numRead;
+            theBuf += numRead;
+            currentAddress += numRead;
+        }
+        return bytesRead;
+    }
+    uint8_t read8(Address address) noexcept override {
+        Address26 curr(address);
+        setChipId(curr);
+        // it will always be lower8 because the read method which called this method already added 1. Thus we can just write a byte with the address we currently have
+        return static_cast<uint8_t>(backingChips[curr.getIndex()].read(address, LoadStoreStyle::Lower8));
+    }
+    uint16_t read16(Address address) noexcept override {
+        Address26 curr(address);
+        setChipId(curr);
+        return backingChips[curr.getIndex()].read(address, LoadStoreStyle::Full16);
+    }
+    void write8(Address address, uint8_t value) noexcept override {
+        Address26 curr(address);
+        setChipId(curr);
+        backingChips[curr.getIndex()].write(curr.getOffset(), value, LoadStoreStyle::Lower8);
+    }
+    void write16(Address address, uint16_t value) noexcept override {
+        Address26 curr(address);
+        setChipId(curr);
+        backingChips[curr.getIndex()].write(curr.getOffset(), value, LoadStoreStyle::Full16);
+        MemoryThing::write16(address, value);
+    }
+private:
+    union Decomposition {
+        constexpr explicit Decomposition(byte value = 0) : index(value) { }
+        constexpr auto getIndex() const noexcept { return index; }
+        byte index;
+        struct {
+            bool s0 : 1;
+            bool s1 : 1;
+            bool s2 : 1;
+        };
+    };
+    inline void setChipId(const Address26& address) noexcept {
+        setChipId(address.getIndex());
+    }
+    void setChipId(byte index) noexcept {
+        if (Decomposition dec(index); dec.getIndex() != currentIndex_.getIndex()) {
+            digitalWrite<Select0>(dec.s0 ? HIGH : LOW);
+            digitalWrite<Select1>(dec.s1 ? HIGH : LOW);
+            digitalWrite<Select2>(dec.s2 ? HIGH : LOW);
+            currentIndex_ = dec;
+        }
+    }
+public:
+    void begin() noexcept override  {
+        if (!initialized_) {
+            Serial.println(F("BRINGING UP SRAM MEMORY BLOCK"));
+            initialized_ = true;
+            currentIndex_.index = 0;
+            setChipId(0);
+            for (int i = 0; i < 8; ++i) {
+                setChipId(i);
+                backingChips[i].begin();
+                if (!backingChips[i].isAvailable()) {
+                    available_ = false;
+                    break;
+                }
+            }
+            if (available_) {
+                Serial.println(F("Done bringing up sram memory blocks!"));
+            } else {
+                Serial.println(F("DISABLING ONBOARD SRAM ACCESS"));
+            }
+        }
+    }
+    bool respondsTo(Address address) const noexcept override {
+        return available_ && MemoryThing::respondsTo(address);
+    }
+private:
+    bool available_ = true;
+    bool initialized_ = false;
+    Decomposition currentIndex_;
+    SingleChip backingChips[NumChips];
+};
 
 using OnboardPSRAM = PSRAMChip<i960Pinout::PSRAM_EN>;
+using OnboardPSRAMBlock = PSRAMBlock8<i960Pinout::PSRAM_EN>;
 #endif //I960SXCHIPSET_PSRAMCHIP_H
