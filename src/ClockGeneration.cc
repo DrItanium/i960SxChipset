93e59c2e4e351596e390adcff183703c8806d665
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-08-08 18:45:08 -0700

    setup clock source for raspberry pi pico

--- src/ClockGeneration.cc
+++ src/ClockGeneration.cc
@@ -3,8 +3,11 @@
 //
 
 #include <Arduino.h>
+#include "Pinout.h"
 #include "ClockGeneration.h"
-
+#ifdef ARDUINO_ARCH_RP2040
+#include "hardware/clocks.h"
+#endif
 void setupClockSource() noexcept {
 #ifdef ARDUINO_GRAND_CENTRAL_M4
 constexpr auto ClockSignal_20MHz = 6;
@@ -23,5 +26,12 @@ constexpr auto pinIndex0 = static_cast<int>(i960Pinout::CLOCK_OUT);
 PORT->Group[g_APinDescription[pinIndex0].ulPort].PINCFG[g_APinDescription[pinIndex0].ulPin].bit.PMUXEN = 1;
 // enable on pin 36 or PA17
 PORT->Group[g_APinDescription[pinIndex0].ulPort].PMUX[g_APinDescription[pinIndex0].ulPin >> 1].reg |= PORT_PMUX_PMUXO(MUX_PA17M_GCLK_IO3);
+#elif defined(ARDUINO_RASPBERRY_PI_PICO)
+// divide 133mhz by 6 to get ~ 20MHz (really 20.8 or so)
+    clock_gpio_init(static_cast<int>(i960Pinout::CLOCK_OUT), CLOCKS_CLK_GPOUT0_CTRL_AUXSRC_VALUE_CLK_SYS, 6);
+#elif defined(ARDUINO_AVR_ATmega1284)
+    // clock source is configured using fuses
+#else
+#warning "setupClockSource is has a default implementation"
 #endif
 }
