be512735940df812499a36007ffef7877471fc2a
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-08-12 07:41:40 -0700

    Include boot.dat as well

--- include/hitagimon.h
+++ include/hitagimon.h
@@ -5,5 +5,7 @@
 #ifndef SXCHIPSET_HITAGIMON_H
 #define SXCHIPSET_HITAGIMON_H
 extern unsigned char boot_rom[];
+extern unsigned char boot_dat[];
 extern unsigned int boot_rom_length;
+extern unsigned int boot_dat_length;
 #endif //SXCHIPSET_HITAGIMON_H
