9ebb0e5b9e13a7d6910041fe950be9d63bcc69ec
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-08-12 07:28:53 -0700

    An an experiment, embed a copy of hitagimon directly into onboard flash

---
+++ include/hitagimon.h
@@ -0,0 +1,9 @@
+//
+// Created by jwscoggins on 8/12/21.
+//
+
+#ifndef SXCHIPSET_HITAGIMON_H
+#define SXCHIPSET_HITAGIMON_H
+extern unsigned char boot_rom[];
+extern unsigned int boot_rom_length;
+#endif //SXCHIPSET_HITAGIMON_H
