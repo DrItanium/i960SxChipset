9ce87e9c986c6378dd2bf03b9b1bb84372375779
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-08-12 11:39:03 -0700

    const pointer to const byte related operations

--- include/OnChipMemoryThing.h
+++ include/OnChipMemoryThing.h
@@ -6,18 +6,12 @@
 #define SXCHIPSET_ONCHIPMEMORYTHING_H
 #include "MCUPlatform.h"
 #include "MemoryThing.h"
-class OnChipMemoryThing : public MemoryThing {
+class ReadOnlyOnChipMemoryThing : public MemoryThing {
 public:
     using Parent = MemoryThing;
-    OnChipMemoryThing(Address baseAddress, Address length, byte* pointer) : Parent(baseAddress, length), pointer_(pointer) { }
-    ~OnChipMemoryThing() override = default;
+    ReadOnlyOnChipMemoryThing(Address baseAddress, Address length, const byte* pointer) : Parent(baseAddress, length), pointer_(pointer) { }
+    ~ReadOnlyOnChipMemoryThing() override = default;
 public:
-    size_t blockWrite(Address address, uint8_t *buf, size_t capacity) noexcept override {
-        for (size_t i = 0; i < capacity; ++i, ++address) {
-            write8(address, buf[i]);
-        }
-        return capacity;
-    }
     size_t blockRead(Address address, uint8_t *buf, size_t capacity) noexcept override {
         for (size_t i = 0; i < capacity; ++i, ++address) {
             buf[i] = read8(address);
@@ -30,33 +24,10 @@ public:
     uint16_t read16(Address address) noexcept override {
         return SplitWord16(pointer_[address], pointer_[address+1]).getWholeValue();
     }
-    void write8(Address address, uint8_t value) noexcept override {
-        pointer_[address] = value;
-    }
-    void write16(Address address, uint16_t value) noexcept override {
-        SplitWord16 decomp(value);
-        pointer_[address] = decomp.bytes[0];
-        pointer_[address+1] = decomp.bytes[1];
-    }
     [[nodiscard]] bool bypassesCache() const noexcept override { return true; }
 private:
-    byte* pointer_ = nullptr;
+    const byte* pointer_ = nullptr;
 };
 
-class ReadOnlyOnChipMemoryThing : public OnChipMemoryThing {
-public:
-    using Parent = OnChipMemoryThing;
-    ReadOnlyOnChipMemoryThing(Address base, Address length, const byte* pointer) : Parent(base, length, const_cast<byte*>(pointer)) { }
-    ~ReadOnlyOnChipMemoryThing() override = default;
-    size_t blockWrite(Address address, uint8_t *buf, size_t capacity) noexcept override {
-        return 0;
-    }
-    void write8(Address address, uint8_t value) noexcept override {
-        // do nothing
-    }
-    void write16(Address address, uint16_t value) noexcept override {
-        // do nothing
-    }
-};
 
 #endif //SXCHIPSET_ONCHIPMEMORYTHING_H
