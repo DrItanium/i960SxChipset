442a4db4b9b313d6f2287b0ab50400ab26bd87a5
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-08-14 22:11:55 -0700

    Do two sets of transactions per loop body

--- src/i960SxChipset.cpp
+++ src/i960SxChipset.cpp
@@ -615,7 +615,64 @@ void loop() {
     // when we do the transition, record the information we need
     processorInterface.newDataCycle();
     auto theThing = getThing(processorInterface.getAddress());
-    if (theThing->bypassesCache()) {
+    bool bypassesCache = theThing->bypassesCache();
+    if (bypassesCache) {
+        if (DigitalPin<i960Pinout::W_R_>::isAsserted()) {
+            do {
+                processorInterface.setDataBits(theThing->read(processorInterface.getAddress(), processorInterface.getStyle()));
+                auto isBurstLast = DigitalPin<i960Pinout::BLAST_>::isAsserted();
+                DigitalPin<i960Pinout::Ready>::pulse();
+                if (isBurstLast) {
+                    break;
+                }
+                processorInterface.burstNext();
+            } while (true);
+        } else {
+            do {
+                theThing->write(processorInterface.getAddress(), processorInterface.getDataBits(), processorInterface.getStyle());
+                auto isBurstLast = DigitalPin<i960Pinout::BLAST_>::isAsserted();
+                DigitalPin<i960Pinout::Ready>::pulse();
+                if (isBurstLast) {
+                    break;
+                }
+                processorInterface.burstNext();
+            } while (true);
+        }
+    } else {
+        auto& theEntry = getLine(*theThing);
+        if (DigitalPin<i960Pinout::W_R_>::isAsserted()) {
+            do {
+                processorInterface.setDataBits(theEntry.get(processorInterface.getCacheOffsetEntry()).getWholeValue());
+                auto isBurstLast = DigitalPin<i960Pinout::BLAST_>::isAsserted();
+                DigitalPin<i960Pinout::Ready>::pulse();
+                if (isBurstLast) {
+                    break;
+                }
+                processorInterface.burstNext();
+            } while (true);
+        } else {
+            do {
+                theEntry.set(processorInterface.getCacheOffsetEntry(),
+                             processorInterface.getStyle(),
+                             SplitWord16{processorInterface.getDataBits()});
+                auto isBurstLast = DigitalPin<i960Pinout::BLAST_>::isAsserted();
+                DigitalPin<i960Pinout::Ready>::pulse();
+                if (isBurstLast) {
+                    break;
+                }
+                processorInterface.burstNext();
+            } while (true);
+        }
+    }
+    while (DigitalPin<i960Pinout::DEN_>::isDeasserted());
+    // keep processing data requests until we
+    // when we do the transition, record the information we need
+    processorInterface.newDataCycle();
+    if (!theThing->respondsTo(processorInterface.getAddress())) {
+        theThing = getThing(processorInterface.getAddress());
+        bypassesCache = theThing->bypassesCache();
+    }
+    if (bypassesCache) {
         if (DigitalPin<i960Pinout::W_R_>::isAsserted()) {
             do {
                 processorInterface.setDataBits(theThing->read(processorInterface.getAddress(), processorInterface.getStyle()));
