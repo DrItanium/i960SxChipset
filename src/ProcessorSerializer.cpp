6262cfa880f1f4d926f88968df55e4b1574afd3c
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-08-23 19:47:52 -0700

    Delete debug statements

--- src/ProcessorSerializer.cpp
+++ src/ProcessorSerializer.cpp
@@ -131,13 +131,7 @@ uint16_t
 ProcessorInterface::getDataBits() noexcept {
     if (dataLinesDirection_ != 0xFFFF) {
         dataLinesDirection_ = 0xFFFF;
-        //auto oldoutput = read16<ProcessorInterface::IOExpanderAddress::DataLines, MCP23x17Registers::OLAT>();
         writeDirection<ProcessorInterface::IOExpanderAddress::DataLines>(dataLinesDirection_);
-        //auto newOutput = read16<ProcessorInterface::IOExpanderAddress::DataLines, MCP23x17Registers::OLAT>();
-        //Serial.print(F("old output 0x"));
-        //Serial.print(oldoutput, HEX);
-        //Serial.print(F(" -> new output 0x"));
-        //Serial.println(newOutput, HEX);
     }
     return readGPIO16<ProcessorInterface::IOExpanderAddress::DataLines>();
 }
