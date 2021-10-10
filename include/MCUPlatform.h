e864cb44f305fa3642e7619ecf6c273095591da5
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-10-10 14:10:47 -0700

    Eliminate more references to dead concepts

--- include/MCUPlatform.h
+++ include/MCUPlatform.h
@@ -38,155 +38,8 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define PACKED_ATTRIBUTE __attribute__((packed))
 #endif
 
-#ifdef ARDUINO_AVR_ATmega1284
-#ifndef PIN_SERIAL_RX
-#define PIN_SERIAL_RX 8
-#endif // end !defined(PIN_SERIAL_RX)
-#ifndef PIN_SERIAL_TX
-#define PIN_SERIAL_TX 9
-#endif // end !defined(PIN_SERIAL_TX)
-#endif // end defined(ARDUINO_AVR_ATmega1284)
-#define DEFINE_PINOUT_REQUIREMENTS \
-    Count,                         \
-    None,                          \
-    IOEXPANDER_PA0,                \
-    IOEXPANDER_PA1,                \
-    IOEXPANDER_PA2,                \
-   IOEXPANDER_PA3,                 \
-   IOEXPANDER_PA4,                 \
-   IOEXPANDER_PA5,                 \
-   IOEXPANDER_PA6,                 \
-   IOEXPANDER_PA7,                 \
-   IOEXPANDER_PB0,                 \
-   IOEXPANDER_PB1,                 \
-   IOEXPANDER_PB2,                 \
-   IOEXPANDER_PB3,                 \
-   IOEXPANDER_PB4,                 \
-   IOEXPANDER_PB5,                 \
-   IOEXPANDER_PB6,                 \
-   IOEXPANDER_PB7,                 \
-   IOEXPANDER_PC0,                 \
-   IOEXPANDER_PC1,                 \
-   IOEXPANDER_PC2,                 \
-   IOEXPANDER_PC3,                 \
-   IOEXPANDER_PC4,                 \
-   IOEXPANDER_PC5,                 \
-   IOEXPANDER_PC6,                 \
-   IOEXPANDER_PC7,                 \
-   IOEXPANDER_PD0,                 \
-   IOEXPANDER_PD1,                 \
-   IOEXPANDER_PD2,                 \
-   IOEXPANDER_PD3,                 \
-   IOEXPANDER_PD4,                 \
-   IOEXPANDER_PD5,                 \
-   IOEXPANDER_PD6,                 \
-   IOEXPANDER_PD7,                 \
-   IOEXPANDER_PE0,                 \
-   IOEXPANDER_PE1,                 \
-   IOEXPANDER_PE2,                 \
-   IOEXPANDER_PE3,                 \
-   IOEXPANDER_PE4,                 \
-   IOEXPANDER_PE5,                 \
-   IOEXPANDER_PE6,                 \
-   IOEXPANDER_PE7,                 \
-   IOEXPANDER_PF0,                 \
-   IOEXPANDER_PF1,                 \
-   IOEXPANDER_PF2,                 \
-   IOEXPANDER_PF3,                 \
-   IOEXPANDER_PF4,                 \
-   IOEXPANDER_PF5,                 \
-   IOEXPANDER_PF6,                 \
-   IOEXPANDER_PF7,                 \
-   IOEXPANDER_PG0,                 \
-   IOEXPANDER_PG1,                 \
-   IOEXPANDER_PG2,                 \
-   IOEXPANDER_PG3,                 \
-   IOEXPANDER_PG4,                 \
-   IOEXPANDER_PG5,                 \
-   IOEXPANDER_PG6,                 \
-   IOEXPANDER_PG7,                 \
-   IOEXPANDER_PH0,                 \
-   IOEXPANDER_PH1,                 \
-   IOEXPANDER_PH2,                 \
-   IOEXPANDER_PH3,                 \
-   IOEXPANDER_PH4,                 \
-   IOEXPANDER_PH5,                 \
-   IOEXPANDER_PH6,                 \
-   IOEXPANDER_PH7
 
 
-template<typename E>
-constexpr bool attachedToIOExpander(E value) noexcept {
-    switch (value) {
-        case E::IOEXPANDER_PA0:
-        case E::IOEXPANDER_PA1:
-        case E::IOEXPANDER_PA2:
-        case E::IOEXPANDER_PA3:
-        case E::IOEXPANDER_PA4:
-        case E::IOEXPANDER_PA5:
-        case E::IOEXPANDER_PA6:
-        case E::IOEXPANDER_PA7:
-        case E::IOEXPANDER_PB0:
-        case E::IOEXPANDER_PB1:
-        case E::IOEXPANDER_PB2:
-        case E::IOEXPANDER_PB3:
-        case E::IOEXPANDER_PB4:
-        case E::IOEXPANDER_PB5:
-        case E::IOEXPANDER_PB6:
-        case E::IOEXPANDER_PB7:
-        case E::IOEXPANDER_PC0:
-        case E::IOEXPANDER_PC1:
-        case E::IOEXPANDER_PC2:
-        case E::IOEXPANDER_PC3:
-        case E::IOEXPANDER_PC4:
-        case E::IOEXPANDER_PC5:
-        case E::IOEXPANDER_PC6:
-        case E::IOEXPANDER_PC7:
-        case E::IOEXPANDER_PD0:
-        case E::IOEXPANDER_PD1:
-        case E::IOEXPANDER_PD2:
-        case E::IOEXPANDER_PD3:
-        case E::IOEXPANDER_PD4:
-        case E::IOEXPANDER_PD5:
-        case E::IOEXPANDER_PD6:
-        case E::IOEXPANDER_PD7:
-        case E::IOEXPANDER_PE0:
-        case E::IOEXPANDER_PE1:
-        case E::IOEXPANDER_PE2:
-        case E::IOEXPANDER_PE3:
-        case E::IOEXPANDER_PE4:
-        case E::IOEXPANDER_PE5:
-        case E::IOEXPANDER_PE6:
-        case E::IOEXPANDER_PE7:
-        case E::IOEXPANDER_PF0:
-        case E::IOEXPANDER_PF1:
-        case E::IOEXPANDER_PF2:
-        case E::IOEXPANDER_PF3:
-        case E::IOEXPANDER_PF4:
-        case E::IOEXPANDER_PF5:
-        case E::IOEXPANDER_PF6:
-        case E::IOEXPANDER_PF7:
-        case E::IOEXPANDER_PG0:
-        case E::IOEXPANDER_PG1:
-        case E::IOEXPANDER_PG2:
-        case E::IOEXPANDER_PG3:
-        case E::IOEXPANDER_PG4:
-        case E::IOEXPANDER_PG5:
-        case E::IOEXPANDER_PG6:
-        case E::IOEXPANDER_PG7:
-        case E::IOEXPANDER_PH0:
-        case E::IOEXPANDER_PH1:
-        case E::IOEXPANDER_PH2:
-        case E::IOEXPANDER_PH3:
-        case E::IOEXPANDER_PH4:
-        case E::IOEXPANDER_PH5:
-        case E::IOEXPANDER_PH6:
-        case E::IOEXPANDER_PH7:
-            return true;
-        default:
-            return false;
-    }
-}
 template<typename E>
 constexpr bool isValidPin(E pin) noexcept {
     return static_cast<int>(pin) < static_cast<int>(E::Count) &&
@@ -242,13 +95,9 @@ public:
     [[nodiscard]] static constexpr auto getCPUFrequency() noexcept { return F_CPU; }
     [[nodiscard]] static constexpr TargetMCU getMCUTarget() noexcept {
 #ifdef ARDUINO_AVR_ATmega1284
-#ifdef CHIPSET_TYPE1
         return TargetMCU::ATmega1284p_Type1;
 #else
         return TargetMCU::Unknown;
-#endif
-#else
-    return TargetMCU::Unknown;
 #endif
     }
     [[nodiscard]] static constexpr auto onAtmega1284p_Type1() noexcept { return getMCUTarget() == TargetMCU::ATmega1284p_Type1; }
@@ -282,6 +131,7 @@ union SplitWord16 {
 union SplitWord32 {
     // adding this dropped program size by over 500 bytes!
     explicit constexpr SplitWord32(uint32_t value = 0) noexcept : wholeValue_(value) { }
+    constexpr SplitWord32(uint16_t lower, uint16_t upper) noexcept : halves{lower, upper} {}
     constexpr SplitWord32(uint8_t lowest, uint8_t lower, uint8_t higher, uint8_t highest) noexcept : bytes{lowest, lower, higher, highest} {}
     [[nodiscard]] constexpr auto getWholeValue() const noexcept { return wholeValue_; }
     [[nodiscard]] constexpr auto getSignedRepresentation() const noexcept { return signedRepresentation_; }
