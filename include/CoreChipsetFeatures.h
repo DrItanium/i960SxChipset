cfdae0f368d05ed8a2739ebab3ec5b49b7dba8ca
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-10-10 13:17:31 -0700

    Added more debugging

--- include/CoreChipsetFeatures.h
+++ include/CoreChipsetFeatures.h
@@ -500,6 +500,10 @@ public:
             case 40: case 41: case 42: case 43: case 44: case 45: case 46: case 47:
             case 48: case 49: case 50: case 51: case 52: case 53: case 54: case 55:
             case 56: case 57: case 58: case 59: case 60: case 61: case 62: case 63:
+                Serial.print(F("READING FROM FILE 0x"));
+                Serial.println(targetPage - 32, HEX);
+                Serial.print(F("Originally 0x"));
+                Serial.println(targetPage, HEX);
                 return files_[targetPage - 32].read(offset, lss);
             default: return 0;
         }
@@ -514,6 +518,10 @@ public:
             case 40: case 41: case 42: case 43: case 44: case 45: case 46: case 47:
             case 48: case 49: case 50: case 51: case 52: case 53: case 54: case 55:
             case 56: case 57: case 58: case 59: case 60: case 61: case 62: case 63:
+                Serial.print(F("WRITING TO FILE 0x"));
+                Serial.println(targetPage - 32, HEX);
+                Serial.print(F("Originally 0x"));
+                Serial.println(targetPage, HEX);
                 files_[targetPage - 32].write(offset,lss,value);
                 break;
             default: break;
