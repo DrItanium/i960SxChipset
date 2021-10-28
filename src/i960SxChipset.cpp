2030b5d3a82255ccdd99a5597c199ac40cd43655
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-10-28 09:19:59 -0700

    Reactivate extended dirty analysis

--- src/i960SxChipset.cpp
+++ src/i960SxChipset.cpp
@@ -109,7 +109,7 @@ public:
     }
     [[nodiscard]] constexpr bool matches(TaggedAddress addr) const noexcept { return isValid() && (tag.restEqual(addr)); }
     [[nodiscard]] constexpr auto get(byte offset) const noexcept { return data[offset].getWholeValue(); }
-    template<bool extendedDirtyAnalysis = false>
+    template<bool extendedDirtyAnalysis = true>
     void set(byte offset, LoadStoreStyle style, SplitWord16 value) noexcept {
         // while unsafe, assume it is correct because we only get this from the ProcessorSerializer, perhaps directly grab it?
         if constexpr (auto &target = data[offset]; extendedDirtyAnalysis) {
