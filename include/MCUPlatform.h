55d855e3d9d3d7e6c2dccead651f6c7cd612a0b1
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-11-04 13:35:01 -0700

    Added is_same and integral constant

--- include/MCUPlatform.h
+++ include/MCUPlatform.h
@@ -28,6 +28,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef I960SXCHIPSET_MCUPLATFORM_H
 #define I960SXCHIPSET_MCUPLATFORM_H
 #include <Arduino.h>
+#include "type_traits.h"
 #include "DependentFalse.h"
 
 // comment this out to disable sram cache support
@@ -65,6 +66,18 @@ constexpr byte pow2(byte value) noexcept {
         return pow2(value - 1) * 2;
     }
 }
+
+template<byte numBits>
+using ClosestBitValue_t = conditional_t<numBits <= 8, byte,
+                                        conditional_t<numBits <= 16, uint16_t,
+                                        conditional_t<numBits <= 24, uint24_t,
+                                        conditional_t<numBits <= 32, uint32_t, uint64_t>>>>;
+
+static_assert(is_same_v<ClosestBitValue_t<1>, ClosestBitValue_t<4>>);
+static_assert(is_same_v<ClosestBitValue_t<4>, byte>);
+static_assert(is_same_v<ClosestBitValue_t<10>, uint16_t>);
+static_assert(!is_same_v<ClosestBitValue_t<10>, ClosestBitValue_t<4>>);
+
 
 enum class TargetMCU {
     ATmega1284p_Type1,
--- include/type_traits.h
+++ include/type_traits.h
@@ -29,9 +29,33 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef SXCHIPSET_TYPE_TRAITS_H
 #define SXCHIPSET_TYPE_TRAITS_H
 
+template<typename T, T v>
+struct integral_constant {
+    static constexpr T value = v;
+    using value_type = T;
+    using type = integral_constant;
+    constexpr operator value_type() const noexcept { return value; }
+    constexpr value_type operator()() const noexcept { return value; }
+};
+
+template<bool B>
+using bool_constant = integral_constant<bool, B>;
+
+using true_type = bool_constant<true>;
+using false_type = bool_constant<false>;
+
 template<bool result, typename T, typename F> struct conditional { using type = T; };
 template<typename T, typename F> struct conditional<false, T, F>{ using type = F; };
 template<bool B, typename T, typename F>
 using conditional_t = typename conditional<B, T, F>::type;
+
+template<typename A, typename B>
+struct is_same : false_type {};
+
+template<typename A>
+struct is_same<A, A> : true_type { };
+
+template<typename A, typename B>
+inline constexpr bool is_same_v = is_same<A, B>::value;
 
 #endif //SXCHIPSET_TYPE_TRAITS_H
