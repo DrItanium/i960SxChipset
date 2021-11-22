c257ff0c54a1c9c1e5e66a1f85e55bd869eab6f3
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-11-22 15:49:42 -0800

    Move pinout descriptions to a separate file, making maintenance much easier
    Each 1284p based chipset includes the appropriate pinout now

--- include/Pinout.h
+++ include/Pinout.h
@@ -48,43 +48,12 @@ enum class LoadStoreStyle : uint8_t {
 };
 /// @todo fix this pinout for different targets
 enum class i960Pinout : int {
-    PORT_B0 = 0,
-    PORT_B1,
-    PORT_B2,
-    PORT_B3,
-    PORT_B4,
-    PORT_B5,
-    PORT_B6,
-    PORT_B7,
-    PORT_D0,
-    PORT_D1,
-    PORT_D2,
-    PORT_D3,
-    PORT_D4,
-    PORT_D5,
-    PORT_D6,
-    PORT_D7,
-    PORT_C0,
-    PORT_C1,
-    PORT_C2,
-    PORT_C3,
-    PORT_C4,
-    PORT_C5,
-    PORT_C6,
-    PORT_C7,
-    PORT_A0,
-    PORT_A1,
-    PORT_A2,
-    PORT_A3,
-    PORT_A4,
-    PORT_A5,
-    PORT_A6,
-    PORT_A7,
-    Count,
-#ifdef TYPE1_4
+#ifdef CHIPSET_TYPE1
+#include "Type1Pinout.def"
+#elif defined(CHIPSET_TYPE1_4)
 #include "Type1_4Pinout.def"
 #else
-#include "Type1Pinout.def"
+#error "Target Chipset Hardware has no pinout defined"
 #endif
 };
 
