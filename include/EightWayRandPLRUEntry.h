c4444aacf0672126c420876fa4a291acb273dda8
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-11-29 17:52:19 -0800

    Do standard tree plru

--- include/EightWayRandPLRUEntry.h
+++ include/EightWayRandPLRUEntry.h
@@ -33,6 +33,13 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "TaggedCacheAddress.h"
 #include "CacheEntry.h"
 
+/**
+ * @brief Divides a series of 8 cache lines in the set into several subsets where the replacement algorithm uses a combination of random replacement and tree plru
+ * @tparam numTagBits The number of bits that make up a tag overall
+ * @tparam totalBitCount  The total number of bits that make up an address partially serviced by this cache set
+ * @tparam numLowestBits The number of bytes that each cache line will store
+ * @tparam T The backing storage type or where we read from and write to on a cache miss
+ */
 template<byte numTagBits, byte totalBitCount, byte numLowestBits, typename T>
 class EightWayRandPLRUCacheSet {
 public:
@@ -71,27 +78,122 @@ public:
         for (auto& way : ways_) {
             way->clear();
         }
-        mruBits_ = 0;
+        bits_ = 0;
     }
     [[nodiscard]] constexpr auto getWay(size_t index = 0) const noexcept { return ways_[index & WayMask]; }
     void setWay(CacheEntry& way, size_t index = 0) noexcept { ways_[index & WayMask] = &way; }
     [[nodiscard]] constexpr size_t size() const noexcept { return NumberOfWays; }
 private:
     void updateFlags(byte index) noexcept {
-        // when updating flags, invert the mruBits and compare it with the entry from the lookup table.
-        // If it is equal then it means we are about to overflow so just do an assignment. Otherwise do the normal |=
-        // we make an assigment to make sure that the compiler doesn't generate a warning from the invert operation
-        if (byte target = BitMaskTable_Byte[index], inv = ~mruBits_; inv == target) {
-            mruBits_ = target;
-        } else {
-            mruBits_ |= target;
+        static constexpr bool ChooseLeft = false;
+        static constexpr bool ChooseRight = true;
+        switch (index) {
+            case 0: // left, left, left
+                a_ = ChooseLeft;
+                c_ = ChooseLeft;
+                g_ = ChooseLeft;
+                break;
+            case 1: // left, left, right
+                a_ = ChooseRight;
+                c_ = ChooseLeft;
+                g_ = ChooseLeft;
+                break;
+            case 2: // left, right, left
+                b_ = ChooseRight;
+                c_ = ChooseLeft;
+                g_ = ChooseLeft;
+                break;
+            case 3: // left, right, right
+                b_ = ChooseRight;
+                c_ = ChooseRight;
+                g_ = ChooseLeft;
+                break;
+            case 4: // right, left, left
+                d_ = ChooseLeft;
+                f_ = ChooseLeft;
+                g_ = ChooseRight;
+                break;
+            case 5: // right, left, right
+                d_ = ChooseRight;
+                f_ = ChooseLeft;
+                g_ = ChooseRight;
+                break;
+            case 6: // right, right, left
+                e_ = ChooseLeft;
+                f_ = ChooseRight;
+                g_ = ChooseRight;
+                break;
+            case 7: // right, right, right
+                e_ = ChooseRight;
+                f_ = ChooseRight;
+                g_ = ChooseRight;
+                break;
+            default:
+                break;
         }
     }
     [[nodiscard]] constexpr byte getLeastRecentlyUsed() const noexcept {
+        if (g_) {
+            // right
+            if (f_) {
+               // right (e)
+               if (e_) {
+                  // right
+                  return 7;
+               } else {
+                  // left
+                  return 6;
+               }
+            } else {
+               // left (d)
+               if (d_) {
+                  // right
+                  return 5;
+               } else {
+                  // left
+                  return 4;
+               }
+            }
+        } else {
+            // left
+            if (c_) {
+                // right (b)
+                if (b_) {
+                   return 3;
+                   // right
+                } else {
+                    return 2;
+                   // left
+                }
+            } else {
+                // left (a)
+                if (a_) {
+                   // right
+                   return 1;
+                } else {
+                   // left
+                   return 0;
+                }
+            }
+        }
         return LRUTable[mruBits_];
     }
 private:
     CacheEntry* ways_[NumberOfWays] = { nullptr };
+    // This is RandPLRU Tree so we need to organize things correctly, I'm going to try four groups of two
+    union {
+        byte bits_ = 0;
+        struct {
+            // depth first traversal ordering
+            bool a_ : 1;  // left pair of left half
+            bool b_ : 1;  // right pair of left half
+            bool c_ : 1; // parent of a_ and b_ (left half)
+            bool d_ : 1; // left pair of right half
+            bool e_ : 1; // right pair of right half
+            bool f_ : 1; // parent of d_ and e_ (right half)
+            bool g_ : 1; // in a normal tree plru this would be the parent of c_ and f_
+        };
+    };
     byte mruBits_ = 0;
 };
 
