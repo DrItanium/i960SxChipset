f542366d7f9e4429bd5b64a6b486208c24096606
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-12-05 14:17:06 -0800

    informCPU is now always_inline

--- src/i960SxChipset.cpp
+++ src/i960SxChipset.cpp
@@ -95,7 +95,7 @@ L1Cache theCache;
 
 
 
-[[nodiscard]] inline bool informCPU() noexcept {
+[[gnu::always_inline]] [[nodiscard]] inline bool informCPU() noexcept {
     // you must scan the BLAST_ pin before pulsing ready, the cpu will change blast for the next transaction
     auto isBurstLast = DigitalPin<i960Pinout::BLAST_>::isAsserted();
     pulse<i960Pinout::Ready>();
