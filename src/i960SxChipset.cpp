052e6139ab121534bf291a46d1d0b5ca61ab57b3
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-12-05 14:36:18 -0800

    Put pulse into a scope to force ordering in all cases

--- src/i960SxChipset.cpp
+++ src/i960SxChipset.cpp
@@ -95,13 +95,15 @@ L1Cache theCache;
 
 
 
-[[nodiscard]] inline bool informCPU() noexcept {
+[[gnu::always_inline]] [[nodiscard]] inline bool informCPU() noexcept {
     // Forcing inline on this method causes the compiler to generate bad code, using volatile nops to gate operations
     // works somewhat but I want to think about a better method going forward.
     // Right now, the compiler is free to do a call to this method if it so desires
     // you must scan the BLAST_ pin before pulsing ready, the cpu will change blast for the next transaction
     auto isBurstLast = DigitalPin<i960Pinout::BLAST_>::isAsserted();
-    pulse<i960Pinout::Ready>();
+    {
+        pulse<i960Pinout::Ready>();
+    }
     return isBurstLast;
 }
 constexpr auto IncrementAddress = true;
