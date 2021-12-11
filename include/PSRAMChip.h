1cc65c3e088db01c9e78eae279f689655b3c876c
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-12-11 14:51:17 -0800

    The PSRAMChip item should be called type2 psram chip

--- include/PSRAMChip.h
+++ include/PSRAMChip.h
@@ -30,5 +30,5 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define I960SXCHIPSET_PSRAMCHIP_H
 // include all of them at the same time but only one should be active at a given point in time
 #include "Type1PSRAMChip.h"
-#include "DualSPIBusPSRAMChip.h"
+#include "Type2PSRAMChip.h"
 #endif //I960SXCHIPSET_PSRAMCHIP_H
