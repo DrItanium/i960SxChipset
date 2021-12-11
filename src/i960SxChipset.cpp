2e6c1439041d7c6dc2390cbf8a6cd1e5199ff7dd
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-12-11 14:30:12 -0800

    Go back to sdcard as ram for type2
    There is some screwy going on with it

--- src/i960SxChipset.cpp
+++ src/i960SxChipset.cpp
@@ -62,7 +62,7 @@ constexpr auto MaximumNumberOfOpenFiles = 16;
 constexpr auto CompileInAddressDebuggingSupport = false;
 constexpr auto AddressDebuggingEnabledOnStartup = false;
 constexpr auto ValidateTransferDuringInstall = true;
-constexpr auto UsePSRAMForType2 = true;
+constexpr auto UsePSRAMForType2 = false;
 /**
  * @brief When set to true, the interrupt lines the mcp23s17 provides are used to determine which bytes to read
  */
