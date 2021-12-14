e275834a2776a30550b5bb4cc2db9b15f4e5e170
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-12-13 18:37:08 -0800

    Setup all of the important pins

--- aux/PrototypeGrandCentralM4Chipset/PrototypeGrandCentralM4Chipset.ino
+++ aux/PrototypeGrandCentralM4Chipset/PrototypeGrandCentralM4Chipset.ino
@@ -3,13 +3,34 @@
 /*
  * A simple grand central m4 sketch to prototype the i960 chipset interface
  */
+// inputs
+constexpr auto MCU_FAIL = 26;
+constexpr auto BLAST = 27;
+constexpr auto DEN = 28;
+constexpr auto WR = 23;
+constexpr auto BE0 = 37;
+constexpr auto BE1 = 36;
+constexpr auto LINE_INT0 = 35;
+constexpr auto LINE_INT1 = 34;
+constexpr auto LINE_INT2 = 33;
+constexpr auto LINE_INT3 = 32;
+// outputs
+constexpr auto MCU_READY = 16;
+constexpr auto RESET960 = 17;
+constexpr auto GPIO_CS = 53;
 constexpr auto PSRAMEN0 = A0;
 constexpr auto PSRAMEN1 = A1;
 constexpr auto PSRAMEN2 = A2;
 constexpr auto PSRAMEN3 = A3;
-constexpr auto GPIO_CS = 53;
-
+inline void outputPin(byte value) noexcept {
+    pinMode(value, OUTPUT);
+}
+inline void inputPin(byte value) noexcept {
+    pinMode(value, INPUT);
+}
 void setup() {
+    outputPin(RESET960);
+    digitalWrite(RESET960, LOW);
     Serial.begin(115200);
     while(!Serial) {
         delay(100);
@@ -19,17 +40,33 @@ void setup() {
         delay(1000);
     }
     SPI.begin();
+    inputPin(MCU_FAIL);
+    inputPin(BLAST);
+    inputPin(DEN);
+    inputPin(WR);
+    inputPin(BE0);
+    inputPin(BE1);
+    inputPin(LINE_INT0);
+    inputPin(LINE_INT1);
+    inputPin(LINE_INT2);
+    inputPin(LINE_INT3);
     // put your setup code here, to run once:
-    pinMode(PSRAMEN0, OUTPUT);
-    pinMode(PSRAMEN1, OUTPUT);
-    pinMode(PSRAMEN2, OUTPUT);
-    pinMode(PSRAMEN3, OUTPUT);
-    pinMode(GPIO_CS, OUTPUT);
+    outputPin(PSRAMEN0);
+    outputPin(PSRAMEN1);
+    outputPin(PSRAMEN2);
+    outputPin(PSRAMEN3);
+    outputPin(GPIO_CS);
+    outputPin(MCU_READY);
+
     digitalWrite(PSRAMEN0, HIGH);
     digitalWrite(PSRAMEN1, HIGH);
     digitalWrite(PSRAMEN2, HIGH);
     digitalWrite(PSRAMEN3, HIGH);
     digitalWrite(GPIO_CS, HIGH);
+    digitalWrite(MCU_READY, HIGH);
+
+    digitalWrite(RESET960, HIGH);
+    // i960 is active after this point
 }
 
 void loop() {
