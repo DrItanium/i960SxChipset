0b4f7128f68356b1e853f9ffbb706e31048b4419
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-12-13 18:28:45 -0800

    GPIO_CS plus other features

--- aux/PrototypeGrandCentralM4Chipset/PrototypeGrandCentralM4Chipset.ino
+++ aux/PrototypeGrandCentralM4Chipset/PrototypeGrandCentralM4Chipset.ino
@@ -1,6 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SD.h>
+#include <SPI.h>
+/*
+ * A simple grand central m4 sketch to prototype the i960 chipset interface
+ */
+constexpr auto PSRAMEN0 = A0;
+constexpr auto PSRAMEN1 = A1;
+constexpr auto PSRAMEN2 = A2;
+constexpr auto PSRAMEN3 = A3;
+constexpr auto GPIO_CS = 53;
 
+void setup() {
+    Serial.begin(115200);
+    while(!Serial) {
+        delay(100);
+    }
+    SD.begin(SDCARD_SS_PIN);
+    SPI.begin();
+    // put your setup code here, to run once:
+    pinMode(PSRAMEN0, OUTPUT);
+    pinMode(PSRAMEN1, OUTPUT);
+    pinMode(PSRAMEN2, OUTPUT);
+    pinMode(PSRAMEN3, OUTPUT);
+    pinMode(GPIO_CS, OUTPUT);
+    digitalWrite(PSRAMEN0, HIGH);
+    digitalWrite(PSRAMEN1, HIGH);
+    digitalWrite(PSRAMEN2, HIGH);
+    digitalWrite(PSRAMEN3, HIGH);
+    digitalWrite(GPIO_CS, HIGH);
 }
 
 void loop() {
