781dbe71242867ba847e036a36c4422530d84343
Joshua Scoggins <Theoretically.x64@gmail.com>
2022-01-03 09:52:00 -0800

    Drop PSRAM specific information from TaggedAddress
    Also, add missing license to TaggedCacheAddress.h.

--- include/TaggedCacheAddress.h
+++ include/TaggedCacheAddress.h
@@ -1,3 +1,27 @@
+/*
+i960SxChipset
+Copyright (c) 2020-2022, Joshua Scoggins
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 //
 // Created by jwscoggins on 9/19/21.
 //
@@ -25,12 +49,6 @@ union TaggedAddress {
     [[nodiscard]] constexpr auto getLowest() const noexcept { return lowest; }
     [[nodiscard]] constexpr auto getOffset() const noexcept { return lowest; }
     [[nodiscard]] constexpr auto getRest() const noexcept { return rest; }
-    [[nodiscard]] constexpr auto getPSRAMChipId() const noexcept { return offset; }
-    [[nodiscard]] constexpr auto getPSRAMIndex() const noexcept { return psramIndex; }
-    [[nodiscard]] constexpr auto getPSRAMAddress() const noexcept { return psramIndex; }
-    [[nodiscard]] constexpr auto getPSRAMAddress_High() const noexcept { return bytes_[2]; }
-    [[nodiscard]] constexpr auto getPSRAMAddress_Middle() const noexcept { return bytes_[1]; }
-    [[nodiscard]] constexpr auto getPSRAMAddress_Low() const noexcept { return bytes_[0]; }
     [[nodiscard]] TaggedAddress aligned() const noexcept {
         TaggedAddress result(base);
         result.lowest = 0;
@@ -43,10 +61,6 @@ private:
         LowerType lowest : NumLowestBits;
         TagType tagIndex : NumTagBits;
         RestType rest : NumRestBits;
-    };
-    struct {
-        uint24_t psramIndex : 23;
-        byte offset : 3;
     };
     byte bytes_[4];
 };
