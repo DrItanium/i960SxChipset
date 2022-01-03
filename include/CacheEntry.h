6827c0a80a832061b8f4b764afdecdae05223e93
Joshua Scoggins <Theoretically.x64@gmail.com>
2022-01-03 09:49:03 -0800

    Update year

--- include/CacheEntry.h
+++ include/CacheEntry.h
@@ -1,6 +1,6 @@
 /*
 i960SxChipset
-Copyright (c) 2020-2021, Joshua Scoggins
+Copyright (c) 2020-2022, Joshua Scoggins
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
