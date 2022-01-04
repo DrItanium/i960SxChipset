5db27874b79a1a77681ea9fd0b2f6676bf176597
Joshua Scoggins <Theoretically.x64@gmail.com>
2022-01-03 17:34:45 -0800

    Pass the CacheEntry type to newDataCycle from i960SxChipset.cpp

--- src/i960SxChipset.cpp
+++ src/i960SxChipset.cpp
@@ -123,7 +123,7 @@ inline void invocationBody() noexcept {
     // there are only two parts to this code, either we map into ram or chipset functions
     // we can just check if we are in ram, otherwise it is considered to be chipset. This means that everything not ram is chipset
     // and so we are actually continually mirroring the mapping for the sake of simplicity
-    ProcessorInterface::newDataCycle<inDebugMode, decltype(theCache)::CacheEntryMask, useInterrupts>();
+    ProcessorInterface::newDataCycle<inDebugMode, decltype(theCache)::CacheEntry , useInterrupts>();
 }
 template<bool allowAddressDebuggingCodePath, bool useInterrupts>
 [[gnu::always_inline]] inline void doInvocationBody() noexcept {
