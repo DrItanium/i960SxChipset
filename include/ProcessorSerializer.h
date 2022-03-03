4689c6a40d94ea0c6455f906cd5ae157cb1e4766
Joshua Scoggins <Theoretically.x64@gmail.com>
2022-03-03 11:32:02 -0800

    Seems that the uppermost 8-bits of the address are not triggering their associated change interrupt properly so enable port mirroring for that io expander to get around the issue.
    I encountered this long ago as well, I do not know what the cause of it is to be honest.

--- include/ProcessorSerializer.h
+++ include/ProcessorSerializer.h
@@ -298,7 +298,8 @@ public:
             // so do a begin operation on all chips (0b000)
             // set IOCON.HAEN on all chips
             write8<ProcessorInterface::IOExpanderAddress::DataLines, MCP23x17Registers::IOCON, false>(initialIOCONValue_);
-            write8<ProcessorInterface::IOExpanderAddress::Upper16Lines, MCP23x17Registers::IOCON, false>(initialIOCONValue_);
+            // mirror the interrupts for the upper 16-bits, for some reason, the upper most 8-bits are never marked as changed
+            write8<ProcessorInterface::IOExpanderAddress::Upper16Lines, MCP23x17Registers::IOCON, false>(0b0100'1000);
             write8<ProcessorInterface::IOExpanderAddress::Lower16Lines, MCP23x17Registers::IOCON, false>(initialIOCONValue_);
             write8<ProcessorInterface::IOExpanderAddress::MemoryCommitExtras, MCP23x17Registers::IOCON, false>(initialIOCONValue_);
             // immediately pull the i960 into reset as soon as possible
