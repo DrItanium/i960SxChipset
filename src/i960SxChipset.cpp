33682a650dd6472c94a567a0541d0cde70c677e2
Joshua Scoggins <Theoretically.x64@gmail.com>
2022-03-02 19:39:19 -0800

    Enable installation validation

--- src/i960SxChipset.cpp
+++ src/i960SxChipset.cpp
@@ -62,7 +62,7 @@ constexpr auto SDBaseAddress = 0xFD00'0000;
 constexpr auto MaximumNumberOfOpenFiles = 16;
 constexpr auto CompileInAddressDebuggingSupport = false;
 constexpr auto AddressDebuggingEnabledOnStartup = false;
-constexpr auto ValidateTransferDuringInstall = false;
+constexpr auto ValidateTransferDuringInstall = true;
 /**
  * @brief When set to true, the interrupt lines the mcp23s17 provides are used to determine which bytes to read
  */
